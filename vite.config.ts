9cf3ca14407d62d4afe2a7df3fd195f4c486a958
p99051-hash <p99051@gmail.com>
2026-01-01 23:50:23 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  base: '/coffeeMZ/',
-}
-
 export default defineConfig(({ 
   base: '/coffeeMZ/',
   plugins: [react()]
